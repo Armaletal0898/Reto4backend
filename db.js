c0e137ee6441b26a2693eb94b203c4dbb15eaf54
Armaletal0898 <armandovillar0898@gmail.com>
2025-05-09 23:49:32 -0400

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
   host: 'reto4backend-production.up.railway.app',
-  port: "3000",
+  port: "57727",
   user: 'root',
   password: 'wgzPWNygKXznAezuoXHnSQOOsXWRLUJX', // Sin contraseña por defecto en XAMPP
   database: 'registro_app' // Asegúrate de crear esta DB en phpMyAdmin
